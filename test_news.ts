6aba249f1d609cf1238b42557dbc95d5100b10b0
Darshan <desaidarshan@hotmail.com>
2026-03-13 13:31:54 -0400

    Fix: Remove debugging files causing Vercel build failures

--- test_news.ts
+++
@@ -1,8 +0,0 @@
-import yahooFinance from 'yahoo-finance2';
-async function test() {
-    console.log(typeof yahooFinance);
-    console.log(Object.keys(yahooFinance));
-    const res = await yahooFinance.search('market', { newsCount: 5 });
-    console.log(JSON.stringify(res.news, null, 2));
-}
-test();
--- test_yf3.ts
+++
@@ -1,5 +0,0 @@
-import yahooFinance from 'yahoo-finance2';
-async function run() {
-    console.log(yahooFinance);
-}
-run();
